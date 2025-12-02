839e7f66b6abfbb36c71d6745b8ff0b0d8d77b78
kcsic <kcsic2000@gmail.com>
2025-12-02 15:38:29 +0900

    init config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve:{
+    alias:{
+      '@':fileURLToPath(new URL('./src', import.meta.url)),
+      '@assets':fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@components':fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@pages':fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@types':fileURLToPath(new URL('./src/types', import.meta.url)),
+      '@states':fileURLToPath(new URL('./src/states', import.meta.url)),
+      '@hooks':fileURLToPath(new URL('./src/hooks', import.meta.url)),
+      '@apis':fileURLToPath(new URL('./src/apis', import.meta.url)),
+      '@config':fileURLToPath(new URL('./src/config', import.meta.url)),
+      '@utils':fileURLToPath(new URL('./src/utils', import.meta.url)),
+    }
+  },
+  server: {
+    port: parseInt(process.env.VITE_PORT || '9200'), // 환경 변수 활용
+  },
 })
